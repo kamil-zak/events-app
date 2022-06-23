23308e5fb82192434bd937872b4e6daab8799342
Kamil Żak <kamil.zak.pl@outlook.com>
2022-06-23 08:12:16 +0200

    Add sign up description details

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -6,13 +6,13 @@ export class SignUpDto {
   @IsEmail()
   email: string;
 
-  @ApiProperty({ description: 'User password', format: 'password', minLength: 8, maxLength: 30 })
+  @ApiProperty({ description: 'User password (8-30 characters)', format: 'password', minLength: 8, maxLength: 30 })
   @Length(8, 30)
   @IsString()
   password: string;
 
   @ApiProperty({
-    description: 'User avatar in JPG/JPEG/PNG format.',
+    description: 'User avatar in JPG/JPEG/PNG format (max 5MB)',
     required: false,
     type: 'string',
     format: 'binary',
