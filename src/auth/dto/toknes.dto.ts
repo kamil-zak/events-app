abd8827e34ababb7656b7221a98bf106446c234e
Kamil Żak <kamil.zak.pl@outlook.com>
2022-06-23 08:09:05 +0200

    Add token expire time to description

--- src/auth/dto/toknes.dto.ts
+++ src/auth/dto/toknes.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ToknesDto {
-  @ApiProperty({ description: 'Access token' })
+  @ApiProperty({ description: 'Access token (expires in 5 minutes)' })
   accessToken: string;
 
   @ApiProperty({ description: 'Refresh token' })
