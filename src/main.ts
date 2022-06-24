2c8d73b72e46ee5975294a3157726bfce994976a
Kamil Żak <kamil.zak.pl@outlook.com>
2022-06-24 15:11:24 +0200

    Enable whitelist in class-validator

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import config from './config/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
