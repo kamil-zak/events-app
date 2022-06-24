02f225c545a1f1ba0895e1de03a5df67c2ec0bcc
Kamil Żak <kamil.zak.pl@outlook.com>
2022-06-24 15:41:28 +0200

    Modify UserId decorator

--- src/common/decorators/user-id.decorator.ts
+++ src/common/decorators/user-id.decorator.ts
@@ -1,8 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import IUserPayload from 'src/auth/interfaces/user-payload.interface';
 
 const UserId = createParamDecorator((_, context: ExecutionContext) => {
-  const request = context.switchToHttp().getRequest();
-  return request.user.id;
+  const user = context.switchToHttp().getRequest().user as IUserPayload;
+  return user.id;
 });
 
 export default UserId;
